f6f5b73258bf9af99f40f07b9b137279c9aadb14
Andrew Pallant (@LdnDeveloper ) <andrewpallant@gmail.com>
2015-03-27 21:16:16 -0400

    AngularJS Weather
    Angular Weather Application Using Open Weather Map - Created from Rob
    Hurring's Implementation

---
+++ js/angular-weather-service.js
@@ -0,0 +1,85 @@
+﻿app.factory("GeolocationService", ['$q', '$window', '$rootScope', function ($q, $window, $rootScope) {
+    return function () {
+        var deferred = $q.defer();
+
+        if (!$window.navigator) {
+            deferred.reject("Geolocation is not supported");
+        } else {
+            $window.navigator.geolocation.getCurrentPosition(function (position) {
+                $rootScope.$apply(function () {
+                    deferred.resolve(position);
+                });
+            });
+        }
+
+        return deferred.promise;
+    }
+}]);
+
+app.factory("WeatherService", ['$q', '$http', function ($q, $http) {
+    var SERVICE_ENDPOINT = "http://api.openweathermap.org/data/2.5";
+    var JSON_P_SUFFIX = "&callback=JSON_CALLBACK"
+
+    var request = function (path) {
+        var deferred = $q.defer();
+
+        $http.jsonp(SERVICE_ENDPOINT + path + JSON_P_SUFFIX).
+          success(function (data, status, headers, config) {
+              deferred.resolve(data);
+          }).
+          error(function (data, status, headers, config) {
+              deferred.reject(data);
+          })
+
+        return deferred.promise;
+    };
+
+    var normalizeDays = function (days) {
+        var d = days;
+
+        if (days === undefined || days === null || parseInt(days) < 1) d = 7;
+        if (d > 14) d = 14; // max 14 days
+
+        return d;
+    };
+
+    return {
+        current: {
+            byPosition: function (lat, lng) {
+                var path = "/weather?lat=" + lat + "&lon=" + lng;
+                return request(path);
+            },
+            byCity: function (cityName) {
+                var path = "/weather?q=" + cityName;
+                return request(path);
+            },
+            byCityId: function (cityId) {
+                var path = "/weather?id=" + cityId;
+                return request(path);
+            }
+        },
+        forecast: {
+            byPosition: function (lat, lng, days, units) {
+                var u = units || 'imperial'; // internal, metric, or imperial
+                var d = normalizeDays(days);
+
+                var path = "/forecast/daily?lat=" + lat + "&lon=" + lng + "&cnt=" + d + "&mode=json";
+                return request(path);
+            },
+            byCity: function (cityName, days, units) {
+                var u = units || 'imperial'; // internal, metric, or imperial
+                var d = normalizeDays(days);
+
+                var path = "/forecast/daily?q=" + cityName + "&cnt=" + d + "&mode=json";
+                return request(path);
+            },
+            byCityId: function (cityId, days, units) {
+                var u = units || 'imperial'; // internal, metric, or imperial
+                var d = normalizeDays(days);
+
+                var path = "/forecast/daily?id=" + cityId + "&cnt=" + d + "&mode=json";
+                return request(path);
+            }
+        }
+    }
+}]);
---
+++ js/usage.js
@@ -0,0 +1,84 @@
+﻿app.directive('roundConverter2', function() {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function(scope, elem, attrs, ngModelCtrl) {
+            function roundNumber(val) {
+                var parsed = parseFloat(val, 10);
+                if(parsed !== parsed) { return null; } // check for NaN
+                var rounded = Math.round(parsed).toFixed(1);
+                return rounded;
+            }
+            ngModelCtrl.$parsers.push(roundNumber); // Parsers take the view value and convert it to a model value.
+        }
+    };
+});
+
+app.controller('MainCtrl1', [
+  '$scope',
+  'GeolocationService',
+  'WeatherService',
+  function ($scope, geolocation, weather) {
+
+      $scope.position = null;
+      $scope.weather = null;
+      $scope.forecast = null;
+      $scope.city = 'London, On';
+
+      // SEE: https://gist.github.com/robhurring/6074128
+      geolocation().then(function (position) {
+          $scope.position = position;
+      });
+
+      // City Watch
+      $scope.$watch('city', function (val) {
+          if (val == '-- Select --' && $scope.position)
+          {
+              // If we have a position and no city - auto look up
+              weather.current.byPosition($scope.position.coords.latitude, $scope.position.coords.longitude).then(function (data) {
+                  $scope.weather = data;
+              });
+
+              weather.forecast.byPosition($scope.position.coords.latitude, $scope.position.coords.longitude).then(function (data) {
+                  $scope.forecast = data;
+              });
+          }
+          else{
+          weather.current.byCity(val).then(function (data) {
+              $scope.weather = data;
+          });
+
+          weather.forecast.byCity(val).then(function (data) {
+              $scope.forecast = data;
+          });
+          }
+      });
+
+      // Auto Detect Position
+      $scope.$watch('position', function (position) {
+          if (!position) {
+              // If no Position Use City
+              weather.current.byCity($scope.city).then(function (data) {
+                  $scope.weather = data;
+              });
+
+              weather.forecast.byCity($scope.city).then(function (data) {
+                  $scope.forecast = data;
+              });
+          } else {
+              $scope.city = '-- Select --';
+              weather.current.byPosition(position.coords.latitude, position.coords.longitude).then(function (data) {
+                  $scope.weather = data;
+              });
+
+              weather.forecast.byPosition(position.coords.latitude, position.coords.longitude).then(function (data) {
+                  $scope.forecast = data;
+              });
+          }
+      });
+
+      
+      
+
+  }]);
+
